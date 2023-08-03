9295f5ede24556f44a9d295befc3126a9f5ade3e
promto-c <prommm01@gmail.com>
2023-08-04 01:58:18 +0700

    update

--- setup.py
+++ setup.py
@@ -3,8 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name='tabler-qicon',
     version='0.1.0',
+    description='Python package that provides easy access to Tabler Icons for PyQt and PySide applications.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Charin Rungchaowarat',
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
+    license='MIT',
     extras_require={
         'PyQt5': ['PyQt5'],
         'PyQt6': ['PyQt6'],
